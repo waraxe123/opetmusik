ccc43d12481f4247409108e1a8b461405f17737c
opetspeed <114659676+opetspeed@users.noreply.github.com>
2022-10-05 16:07:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "13237674"))
-API_HASH = os.getenv("API_HASH", "6dab53715c41d7dc14e5d249aa95f704")
-SESSION = os.getenv("SESSION", "BQAYu453SK4amN9267sRgm8YuCWhmhB5b8ReEC7LJax4SAusBzmXwdqAcxrt2xRGZjEOXCYubCv7seBibauCGwimOlKuPvXh7iLMtTwGETLCaEuD_vhzxuVhlrZJkoA7POoAGyO_hG2q94grVS5U79rqUa8sG38NX9DQM6C9AdRafaPg1Sg76SdYkOa0hxNG8zYODfJU8wysNO5XNhrZQ_ij98g51BGOltJkBn_SO1lgtHVUnKdOXcyJFJDICUo1lRScIK5o4yDdh2i4hOeTWf0lE6DQYkrcpEc3KRbnFiqNjli6VzIs0qTcYRXe8ADZOBHZ-2GCl5i8PzcvrJ1N9jUNAAAAAU3NrZcA")
+API_ID = int(os.getenv("API_ID", "18124377"))
+API_HASH = os.getenv("API_HASH", "b4a24da3bf670fefe3c0e719e1569b14")
+SESSION = os.getenv("SESSION", "BQAQe9OEAuwzpmMXTDmlMSrDSRlG8nhxbRJDLlpMAEdR-4SCYh3lHo-cj6znHipduWYB2o3EMgFeCnCl8cw_1oFxR-088JbkKdsAw_s7kUopUy_M8oXpM2cTXXbtFXPbOZ9Pb1vR_MwlsIyQriMYUrPmbnSf2gsy1SwpTGcfe1CRBZem9vSez7w9udZ71pqvDthqoA564HoNW-tQXRjqkIgmOqgWtVTsrpWKf2kA0Y6140OouEuHiYMGth2vDqL8MwBLaUlt_8CNod2SvRs7k1iPXjWex58g9MVI2sUzwoW3twJyMmq196dbw5u1floxrOch0NV8MmJRTTZoqqdyLT4pAAAAAVTZ1jYA")
 HNDLR = os.getenv("HNDLR", "✓")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "2027429081").split()))
 
